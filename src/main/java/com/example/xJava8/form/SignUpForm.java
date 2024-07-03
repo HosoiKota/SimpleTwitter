ad5124b4a2a6125ef7fed03cd81060273d1a243d
hosoi.kota <hosoi.kota@alhinc.jp>
2024-07-03 10:21:52 +0900

    バグ修正
    ・signup画面でパスワードのバリデーションを修正

--- src/main/java/com/example/xJava8/form/SignUpForm.java
+++ src/main/java/com/example/xJava8/form/SignUpForm.java
@@ -2,6 +2,7 @@ package com.example.xJava8.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -20,9 +21,20 @@ public class SignUpForm {
 
     @NotNull
     @Pattern(regexp = "^[\\s\\S]*[^(\\s|　)]+[\\s\\S]*$")
-    @Size(min = 6, max = 20)
     private String password;
 
     @Size(max = 100)
     private String description;
+
+    @AssertTrue(message = "パスワードは6文字以上20文字以下で入力してください。")
+    public boolean isPasswordValid() {
+
+        if (password == null || !password.matches("^[\\s\\S]*[^(\\s|　)]+[\\s\\S]*$")) {
+            return true;
+        } else if (password.length() <= 6 || 20 <= password.length()) {
+            return false;
+        }
+        return true;
+    }
+
 }
