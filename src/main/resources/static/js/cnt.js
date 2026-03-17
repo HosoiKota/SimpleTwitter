41f53a425d7f641fb79a68cbe33f4460f5f2f392
hosoi.kota <hosoi.kota@alhinc.jp>
2026-03-18 08:06:46 +0900

    refactor: #6

--- src/main/resources/static/js/cnt.js
+++ src/main/resources/static/js/cnt.js
@@ -1,6 +1,6 @@
 $(function(){
     // tweet comment 入力文字数インジケーター
-    $('.tweet-box').on('input', function(){
+    $('textarea').on('input', function(){
         // 文字数を取得
         let cnt = $(this).val().length;
         let max = 140;
@@ -8,26 +8,26 @@ $(function(){
 
         // 個々のインジケーターを対象に
         const $parent = $(this).parent();
-        const $circle_bar = $parent.find('.circle_bar');
-        const $now_cnt = $parent.find('.now_cnt');
-        const $count_circle = $parent.find('.count_circle');
+        const $circle_bar = $parent.find('.circle-bar');
+        const $now_cnt = $parent.find('.now-cnt');
+        const $count_circle = $parent.find('.count-circle');
 
         $circle_bar.css('--percent', percent);
         $now_cnt.text(cnt);
 
-        const $target = $circle_bar.add('cnt_area');
-        $target.removeClass('cnt_alert cnt_danger');
+        const $target = $circle_bar.add('cnt-area');
+        $target.removeClass('cnt-alert cnt-danger');
 
         if (cnt === 0) {
             $count_circle.hide();
         } else {
             $count_circle.show();
             if (cnt >= max) {
-                $target.addClass('cnt_danger');
+                $target.addClass('cnt-danger');
             } else if (cnt >= max - 20) {
-                $target.addClass('cnt_alert');
+                $target.addClass('cnt-alert');
             }
         }
     });
-    $('.tweet-box').trigger('input');
+    $('textarea').trigger('input');
 });
